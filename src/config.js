1921279708d00532fe64f4eab45a0553711502bc
Gildo Chauze <gildochauze@gmail.com>
2026-06-19 09:09:29 +0200

    Site icon added

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const apiKey = process.env.REACT_APP_apiKey;
-export const authDomain = process.env.REACT_APP_authDomain;
-export const projectId = process.env.REACT_APP_projectId;
-export const storageBucket = process.env.REACT_APP_storageBucket;
-export const messagingSenderId = process.env.REACT_APP_messagingSenderId;
-export const appId = process.env.REACT_APP_appId;
-
-
